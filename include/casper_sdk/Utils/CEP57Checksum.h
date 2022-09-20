7450d3b94139294b84cc3349bb7dae85e3f3b166
Piotr Belkner <piotrbelkner@gmail.com>
2022-09-20 15:11:18 +0200

    test2

---
+++ include/casper_sdk/Utils/CEP57Checksum.h
@@ -0,0 +1,34 @@
+#pragma once
+
+#include <sstream>
+#include <stdexcept>
+#include <string>
+
+// Utils
+#include "Utils/CryptoUtil.h"
+
+// CryptoPP
+#include <cryptopp/xed25519.h>
+
+#include "cryptopp/blake2.h"
+#include "cryptopp/cryptlib.h"
+#include "cryptopp/files.h"
+#include "cryptopp/hex.h"
+#include "cryptopp/secblock.h"
+
+namespace Casper {
+class CEP57Checksum {
+ private:
+  static CBytes _bytes_to_nibbles(CBytes bytes);
+
+  static std::vector<bool> _bytes_to_bits_cycle(CBytes bytes);
+
+ public:
+  static bool HasChecksum(std::string hex);
+
+  static std::string Encode(CBytes decoded);
+
+  static CBytes Decode(std::string encoded);
+};
+
+}  // namespace Casper
---
+++ include/casper_sdk/Utils/CryptoUtil.h
@@ -0,0 +1,15 @@
+#pragma once
+
+#include "Base.h"
+
+// Crypto
+#include "cryptopp/secblock.h"
+
+namespace Casper {
+struct CryptoUtil {
+  static CBytes hexDecode(std::string hex);
+
+  static std::string hexEncode(CBytes bytes);
+};
+
+}  // namespace Casper
